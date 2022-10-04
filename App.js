bdad14566dcdd0ae776df61f126a75a259ffcca0
myphuong <lyphuong@nexondv.com>
2022-10-04 17:42:17 +0700

    processing

--- App.js
+++ App.js
@@ -1,37 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState, useEffect } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { GameEngine } from 'react-native-game-engine';
 import entities from './entities';
 import Physics from './physics';
 
 export default function App() {
   const [running, setRunning] = useState(false);
+  const [gameEngine, setGameEngine] = useState(null);
+  const [currentPoints, setCurrentPoints] = useState(null);
 
   useEffect(() => {
-    setRunning(true);
+    setRunning(false);
   }, [])
   
   return (
     <View style={{flex: 1}}>
+      <Text style={{textAlign: 'center', fontSize: 40, fontWeight: 'bold', margin: 20}}>{currentPoints}</Text>
       <GameEngine
+        ref={(ref) => {
+          setGameEngine(ref)
+        }}
         systems={[Physics]}
-        entities={entities()}
+        entities={entities()} 
         running={running}
         style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}
+        onEvent = {(e) => {
+          switch(e.type){
+            case 'game_over':
+              setRunning(false);
+              gameEngine.stop();
+              setCurrentPoints(0);
+              break;
+
+            case 'new_point':
+              setCurrentPoints(currentPoints + 1);
+              break;
+          }
+        }}
       >
 
       </GameEngine>
+      {!running ? 
+      <View></View> 
+    : null  }
       <StatusBar style="auto" />
     </View>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
--- physics.js
+++ physics.js
@@ -13,13 +13,18 @@ const Physics = (entities, {touches, time, dispatch}) => {
     .forEach(element => {
        Matter.Body.setVelocity(entities.Bird.body, {
         x: 0,
-        y: -8
+        y: -5
        });        
     }); 
     Matter.Engine.update(engine, time.delta);
 
     for (let index = 1; index <= 2; index++) {
 
+        if(entities[`ObstacleTop${index}`].body.bounds.max.x <= 50 && !entities[`ObstacleTop${index}`.point])
+        {
+            entities[`ObstacleTop${index}`].point = true;
+            dispatch({type : 'new_point'});
+        }
         if(entities[`ObstacleTop${index}`].body.bounds.max.x <= 0)
         {
             const pipeSizePos = getPipeSizePosPair(windowWidth * 0.9);
@@ -31,6 +36,10 @@ const Physics = (entities, {touches, time, dispatch}) => {
         Matter.Body.translate(entities[`ObstacleTop${index}`].body, {x: -3, y: 0});
         Matter.Body.translate(entities[`ObstacleBottom${index}`].body, {x: -3, y: 0});
     }
+
+    Matter.Events.on(engine, 'collisionStart', (event) => {
+        dispatch({type: 'game_over'})
+    })
 
     return entities;
 }
